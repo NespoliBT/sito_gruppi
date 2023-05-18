4a9cfa75c21207bf922dea3ecbd30cda95a316c9
NespoliBT <nespoli.bt@gmail.com>
2023-05-18 17:59:46 +0200

    immagina scrivere commit sensati

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,19 +1,44 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import Search from "$lib/components/Search/Search.svelte";
+  import { setCookie, getCookie } from "$lib/helpers/cookieHelper";
 
   let header: HTMLDivElement;
+
+  let logo = "/img/logo.png";
+  let darkModeIcon = "";
+
+  onMount(() => {
+    const darkMode = getCookie("darkMode");
+    if (darkMode === "dark") {
+      changeMode();
+    }
+  });
+
+  function changeMode() {
+    darkModeIcon = darkModeIcon === "" ? "" : "";
+    logo = darkModeIcon === "" ? "/img/logo.png" : "/img/logo-scuro.png";
+
+    setCookie("darkMode", darkModeIcon === "" ? "light" : "dark", 365);
+
+    document
+      .querySelector("html")
+      ?.setAttribute("data-theme", darkModeIcon === "" ? "light" : "dark");
+  }
 </script>
 
 <div class="header" bind:this={header}>
   <div class="logo">
-    <img src="/img/logo.png" alt="" />
+    <img src={logo} alt="" />
     <div class="title">
       Studenti <br />
       Indipendenti
     </div>
   </div>
-
   <Search />
+  <div class="dark-mode">
+    <button on:click={() => changeMode()}>{darkModeIcon}</button>
+  </div>
 </div>
 
 <style lang="scss">
